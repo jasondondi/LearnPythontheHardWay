6860b956519f68ab1c1cd0f8550a560cccf9e9a8
Jason Dondi Evans <jsndndi@users.noreply.github.com>
2017-11-12 10:11:33 +0000

    Did the first three chapters of Learn Python the Hard Way

---
+++ ex3.py
@@ -0,0 +1,25 @@
+# Write a line that tells us what you're about to do:
+print "I will now count my chickens:"
+
+# Do math - I don't know why you add hens and divide by 6, but there you go.
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more." 
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
